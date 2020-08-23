faaa5cedb6cc973be3518a63d2fc2525d6add4b8
Yarin Amrani <yrynmrny@h-mbp-sl-yryn.mynet>
2020-08-23 03:48:36 +0300

    finished week 2

---
+++ Day-5/Dom Traversal/main.js
@@ -0,0 +1,20 @@
+// // $(".remove").on("click", function(){
+// //   alert($(this).closest(".post").data().id)
+// // })
+
+// $('button').on('click', function() {
+//   let relevantInputValue = $(this).closest("div").find("input").val()
+//   alert(relevantInputValue)
+// })
+
+
+// // spotcheck 1// 
+// $('button').on("click", function(){
+//   console.log($(this).closest("div").find("span").text())
+// })
+
+//spotcheck 2//
+$("button").on("click", function(){
+  alert($(".container").find("p").text())
+})
+
